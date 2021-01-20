c838809dc53d9cd65d26f61455e843227da61094
Abdinajib Liban <abdinajibliban@gmail.com>
2021-01-19 23:44:35 -0500

    completed tables and database in schema file

--- schema.sql
+++ schema.sql
@@ -1,1 +1,28 @@
----schema file----
+DROP DATABASE IF EXISTS corporateStructure_DB;
+CREATE DATABASE corporateStructure_DB;
+USE corporateStructure_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL (10,2) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREiGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name  VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREiGN KEY (manager_id) REFERENCES role(id) 
+);
